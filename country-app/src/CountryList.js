b968f8c7c713506ae64de68cbfeab9122d3a99e0
younes bourennane <younesbourennane.yb@gmail.com>
2023-06-22 18:49:23 +0100

    add CountryList Component

---
+++ country-app/src/CountryList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './CountryList.css';
+
+
+const CountryList = () => {
+
+  return (
+    <div>
+      <h1>Country List</h1>
+
+    </div>
+  );
+};
+
+export default CountryList;
